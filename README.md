bb2b5613c10724366843bfde29e014ec1b2a4ac3
ElvesSchweppe <112409291+ElvesSchweppe@users.noreply.github.com>
2022-12-27 17:56:04 -0300

    Envio do Projeto - Semana 1 e 2
    Após aprender a utilizar um pouco melhor o Git e GitHub optei por atualizar de modo geral o projeto - deixando mais "educativo".

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # AluraChallenge - Ciência de Dados - AGO22
  Repositório com as atividades do Alura Challenge de Ciência de Dados (AGO/22)
 
-SOBRE O CHALENGE
+    Ao longo do desenvolvimento procurei utilizar uma técnica mais pedagógica de anotações, inspirada no 5w2h, se baseia em registrar antes de cada ação "O QUE?" será feito e "POR QUE?será feito" eu chamo de "OQPQ" - opcionalmente podemos registrar o "COMO?" será feito;
+
+## SOBRE O CHALENGE
 
 **Boas-vindas!**
 
@@ -13,6 +15,23 @@ Desse modo, você solicita um conjunto de dados que contenha as informações de
 
 Por fim, você vai utilizar o **GitHub** e desenvolver um **portfólio** focado em Data Science, Data Analytics e Machine Learning.
 
+## SEMANA 1 - TRATAMENTO DOS DADOS
+    A semana 1 foi focada no tratamento das informações através de SQL
+    A pasta SEM_1 contem os 'DUMPS' que foram dados como ponto de partida. Após fazer a importação no MySQL 
 
-SEMANA 1 - TRELLO
-https://trello.com/invite/b/7OIWYiA3/7eab79cfe8bd478b8857a0bd17f26929/aluracash
+    ### PASSO 1 - Criei o Banco de dados 
+    ### PASSO 2 - importei os DUMPS
+    ### PASSO 3 - Verifiquei superficialmente os dados nas tabelas
+    ### PASSO 4 - Identifiquei o número de linhas de cada tabela
+    ### PASSO 5 - INCONSISTÊNCIAS DIRETAS - Verificando inexistência de dados nas tabelas
+    ### PASSO 6 - INCONSISTÊNCIAS INDIRETAS - Verificando a existência de dados inúteis nas tabelas
+    ### PASSO 7 - INCONSISTÊNCIAS CONCEITUAIS - Verificando a existência de dados anômalos nas tabelas
+    ### PASSO 8 - CORREÇÃO DAS INCONSISTÊNCIAS -- Abordagem 1
+    ### PASSO 9 - TRADUÇÃO DOS DADOS
+    ### PASSO 10 - EXPORT CSV (produziu um arquivo CSV 'TABELA_UNIFICADA_V1.csv')
+    ### PASSO 11 - CORREÇÃO DAS INCONSISTÊNCIAS -- Abordagem 2 
+    ### PASSO 12 - EXPORT CSV (produziu um arquivo CSV 'TABELA_UNIFICADA_V2.csv')
+
+
+## SEMANA 2 - PREPARAÇÃO DOS DADOS E DEFINIÇÃO DO MODELO DE MACHINE LEARNING
+
---
+++ SEM_1/Resposta_semana1.sql
@@ -0,0 +1,398 @@
+## PREPARAÇÃO
+/*Os DUMPS foram baixados e importados, estou utilizando o MySQL Workbench*/
+/*As verificações, anotações e registros estão sendo submetidas no Trello e serão compartilhadas no GitHub*/
+
+/*Utilizarei uma técnica mais pedagógica de anotações inspirada no 5w2h se baseia em registrar antes de cada ação "O QUE?" "POR QUE?" eu chamo de "OQPQ" opcionalmente podemos registrar o "COMO?"*/
+
+## PASSO 1 - Criei o Banco de dados 
+/*OQPQ 1
+O QUE - Criar o Banco de dados dentro do MySQL (importante ter o mesmo nome do banco a ser importado).
+PORQUE - Para que possamos alocar e trabalhar com os dados que serao importados.
+COMO - através de comando "CREATE DATABASE IF NOT EXISTS"
+*/
+
+CREATE DATABASE IF NOT EXISTS analise_risco;
+USE analise_risco;
+
+## PASSO 2 - importei os DUMPS
+/*OQPQ 2
+O QUE - Importar e alocar os dados no banco criado.
+PORQUE - Para ter acesso e trabalhar com os dados.
+COMO - utilizei os menus e atalhos Server>Data Import.
+*/
+
+## PASSO 3 - Verifiquei superficialmente os dados nas tabelas
+/*OQPQ 3
+O QUE - Após a importação verificar as tabelas e os dados registrados
+PORQUE - É importante garantir a existencia e reconhecer o tipo de dados que estamos trabalhando.
+COMO - Através de comando para a exibição (SELECT FROM) das tabelas presentes (verifiqueis os nomes em 'Tables' no 'Navigator')
+*/
+SELECT * FROM dados_mutuarios;
+SELECT * FROM emprestimos;
+SELECT * FROM historicos_banco;
+SELECT * FROM ids;
+
+
+## PASSO 4 - Identifiquei o número de linhas de cada tabela
+/*OQPQ 4
+O QUE - Reconhecer o tamanho dos bancos de dados
+PORQUE - Saber a quantidade de dados em que se está trabalhado pode ser essencial na definição de estratégias de ação
+COMO - Usei o metodo COUNT(*)
+*/
+SELECT(
+	SELECT COUNT(*) FROM dados_mutuarios) AS mutuario,
+    (SELECT COUNT(*) FROM emprestimos) AS emprestimo,
+    (SELECT COUNT(*) FROM historicos_banco) AS historico,
+    (SELECT COUNT(*) FROM ids) AS ids;
+
+
+## PASSO 5 - INCONSISTÊNCIAS DIRETAS - Verificando inexistência de dados nas tabelas
+/*OQPQ 5
+O QUE - Verificar se há campos 'nulos' ou 'vazios'
+PORQUE - Na proposta em questão é preciso que haja integridade nas informações para que possamos chegar em um modelo preditivo.
+COMO - Usei o metodo COUNT(*) onde as colunas são nulas ('IS NULL') e onde são vazias "=''"
+*/
+
+## Passo 5.1 - dados_mutuarios
+SELECT(
+	SELECT COUNT(*) FROM dados_mutuarios WHERE person_id IS NULL) AS ID,
+    (SELECT COUNT(*) FROM dados_mutuarios WHERE person_age IS NULL) AS age,
+    (SELECT COUNT(*) FROM dados_mutuarios WHERE person_income IS NULL) AS income,
+    (SELECT COUNT(*) FROM dados_mutuarios WHERE person_home_ownership IS NULL) AS home,
+    (SELECT COUNT(*) FROM dados_mutuarios WHERE person_emp_length IS NULL) AS emp_length;
+## Passo 5.2 - emprestimos
+SELECT(
+	SELECT COUNT(*) FROM emprestimos WHERE loan_id IS NULL) AS ID,
+    (SELECT COUNT(*) FROM emprestimos WHERE loan_intent IS NULL) AS intent,
+    (SELECT COUNT(*) FROM emprestimos WHERE loan_grade IS NULL) AS grade,
+    (SELECT COUNT(*) FROM emprestimos WHERE loan_amnt IS NULL) AS amnt,
+    (SELECT COUNT(*) FROM emprestimos WHERE loan_int_rate IS NULL) AS int_rate,
+    (SELECT COUNT(*) FROM emprestimos WHERE loan_status IS NULL) AS loan_status,
+    (SELECT COUNT(*) FROM emprestimos WHERE loan_percent_income IS NULL) AS percent;
+## Passo 5.3 - historicos_banco
+SELECT(
+	SELECT COUNT(*) FROM historicos_banco WHERE cb_id IS NULL) AS ID,
+    (SELECT COUNT(*) FROM historicos_banco WHERE cb_person_default_on_file IS NULL) AS default_on_file,
+    (SELECT COUNT(*) FROM historicos_banco WHERE cb_person_cred_hist_length IS NULL) AS cred_hist;
+
+## Passo 5.4 - ids
+SELECT(
+	SELECT COUNT(*) FROM ids WHERE person_id IS NULL) AS person,
+    (SELECT COUNT(*) FROM ids WHERE loan_id IS NULL) AS loan,
+    (SELECT COUNT(*) FROM ids WHERE cb_id IS NULL) AS cb;
+
+## PASSO 6 - INCONSISTÊNCIAS INDIRETAS - Verificando a existência de dados inúteis nas tabelas
+## Passo 6.1 - dados_mutuarios (procurando ' ' - vazios não nulos)
+SELECT(
+	SELECT COUNT(*) FROM dados_mutuarios WHERE person_id ='') AS ID,
+    (SELECT COUNT(*) FROM dados_mutuarios WHERE person_age ='') AS age,
+    (SELECT COUNT(*) FROM dados_mutuarios WHERE person_income ='') AS income,
+    (SELECT COUNT(*) FROM dados_mutuarios WHERE person_home_ownership ='') AS home,
+    (SELECT COUNT(*) FROM dados_mutuarios WHERE person_emp_length ='') AS emp_length;
+
+## Passo 6.2 - emprestimos
+SELECT(
+	SELECT COUNT(*) FROM emprestimos WHERE loan_id = '' ) AS ID,
+    (SELECT COUNT(*) FROM emprestimos WHERE loan_intent = '') AS intent,
+    (SELECT COUNT(*) FROM emprestimos WHERE loan_grade = '') AS grade,
+    (SELECT COUNT(*) FROM emprestimos WHERE loan_amnt = '') AS amnt,
+    (SELECT COUNT(*) FROM emprestimos WHERE loan_int_rate = '') AS int_rate,
+    (SELECT COUNT(*) FROM emprestimos WHERE loan_status = '') AS loan_status,
+    (SELECT COUNT(*) FROM emprestimos WHERE loan_percent_income = '') AS percent_income;
+
+## Passo 6.3 - historicos_banco
+SELECT(
+	SELECT COUNT(*) FROM historicos_banco WHERE cb_id = '') AS ID,
+    (SELECT COUNT(*) FROM historicos_banco WHERE cb_person_default_on_file ='') AS default_on_file,
+    (SELECT COUNT(*) FROM historicos_banco WHERE cb_person_cred_hist_length = '') AS cred_hist;
+
+## Passo 6.4 - ids
+SELECT(
+	SELECT COUNT(*) FROM ids WHERE person_id ='') AS person,
+    (SELECT COUNT(*) FROM ids WHERE loan_id ='') AS loan,
+    (SELECT COUNT(*) FROM ids WHERE cb_id ='') AS cb;
+
+## PASSO 7 - INCONSISTÊNCIAS CONCEITUAIS - Verificando a existência de dados anômalos nas tabelas
+
+/*OQPQ 6
+O QUE - Além de dados inexistentes as tabelas podem conter dados inúteis ou suspeitos
+PORQUE - Para que tenhamos um bom modelo preditivo precisamos 'limpar' as bases que serão usadas para teste e treino, do contrario poderemos fazer previsões enviesadas ou equivocadas.
+COMO - Delimitei o que seriam dados suspeitos em cada tabela e utilzei o count.
+*/
+
+## Passo 7.1 - dados_mutuarios
+##(Para os dados pessoais crianças ou pessoas muito idosas, ou ainda pessoas que estão empregadas a mais tempo do que tem idade, ou mesmo que começaram a trabalhar enquanto crianças)
+##(Utilizei - criança abaixo de 13 anos e idades suspeitas acima de 100)
+SELECT(
+	SELECT COUNT(*) FROM dados_mutuarios WHERE person_age >100) AS mais_de_100_anos,
+    (SELECT COUNT(*) FROM dados_mutuarios WHERE person_age <13) AS criancas,
+    (SELECT COUNT(*) FROM dados_mutuarios WHERE ((person_age-person_emp_length)>0 AND (person_age-person_emp_length) < 13)) AS trabalho_infantil,
+    (SELECT COUNT(*) FROM dados_mutuarios WHERE (person_age-person_emp_length)<0) AS servico_acima_idade;
+
+## Passo 7.2 - emprestimos
+##(Para os dados de emprestimos podem ser anômalos empréstimos com valor 'zerado' ou 'sem taxa de juros' o que já foi verificado como Inconsistencia Indireta no passo 6.2) 
+##(A coluna 'loan_percent_income'  também pode trazer distorções se for permitida a edição ou inserção dos dados de renda, valor emprestado e comprometimento em separado e se não for gerida por uma fórmula)
+SELECT tab_unica.* FROM
+(SELECT 
+ids.person_id,
+dados_mutuarios.person_income,
+emprestimos.loan_amnt,
+emprestimos.loan_percent_income,
+ROUND(emprestimos.loan_amnt / dados_mutuarios.person_income,2) as loan_percent_calculado
+
+FROM
+/*Tabelas Unificadas*/
+((ids 
+INNER JOIN 
+dados_mutuarios
+ON
+ids.person_id = dados_mutuarios.person_id)
+INNER JOIN
+emprestimos
+ON
+ids.loan_id = emprestimos.loan_id)) tab_unica
+WHERE (person_id <> '' AND loan_percent_calculado <> loan_percent_income );
+
+## Passo 7.3 - historicos_banco
+##(Para os históricos podemos considerar anômalos distorções, se for permitida a edição ou inserção dos dados de idade e tempo de crédito em separado, podemos ter históricos de crédito maiores que a idade)
+SELECT tab_unica.* FROM
+(SELECT 
+ids.person_id,
+dados_mutuarios.person_age,
+historicos_banco.cb_person_cred_hist_length,
+(dados_mutuarios.person_age - historicos_banco.cb_person_cred_hist_length) as idade_primeiro_credito
+
+FROM
+/*Tabelas Unificadas*/
+((ids 
+INNER JOIN 
+dados_mutuarios
+ON
+ids.person_id = dados_mutuarios.person_id)
+INNER JOIN
+historicos_banco
+ON
+ids.cb_id = historicos_banco.cb_id)) tab_unica
+WHERE (person_age - cb_person_cred_hist_length <13);
+
+
+##PASSO 8 - CORREÇÃO DAS INCONSISTÊNCIAS -- Abordagem 1
+## Como não havia uma orientação inicial do que seria a "correção dos dados" optei por não eliminar nada, num primeiro momento, criei duas colunas adicionais uma traz a informação se existe algum campo nulo no cadastro, a outra traz a informação se existe algum ponto a ser observado;
+## A unica exceção são as quatro chaves vazias em 'ids.person_id' que ao se cruzarem com a 'dados_mutuarios' no 'inner join' causam 12 resultados adicionais;
+## Aproveitei para gerar a exibição que será exportada com o nomes das colunas traduzidos;
+
+/*OQPQ 7
+O QUE - Indicar as linhas com dados faltantes ou suspeitos para decisão sobre correção ou exclusão posteriores;
+PORQUE - Para 'limpar' a base de dados e eliminar viés, ou falsas conclusões;
+COMO - Defini uma função com base nas quatro inconscistências conceituais possíveis e usei um método de seleção e concatenação com base em um modelo de verificação exponencial (estilo somatório mas com base 10).
+*/
+
+DELETE FROM ids where ids.person_id = ''; /*removidas as 4 chaves*/
+
+## Para facilitar o processo criei uma função que vai associar os campos a serem observados na presença de dados 'estranhos' na coluna de observações
+DELIMITER $$
+CREATE FUNCTION observar(numero INT) RETURNS TEXT
+BEGIN
+	DECLARE passo INT DEFAULT 3;
+    DECLARE subtrator INT DEFAULT numero;
+    DECLARE resposta TEXT DEFAULT '';
+    WHILE passo >=0 DO
+		CASE WHEN (subtrator-POWER(10,passo)>=0) THEN
+			SET subtrator = subtrator - POWER(10,passo);
+			SET resposta = CONCAT(resposta,
+				CASE WHEN passo = 3 THEN 'Idade maior que 100 ou menor que 13; '
+					WHEN passo = 2 THEN 'Tempo de serviço incompatível com a idade; '
+                    WHEN passo = 1 THEN 'Cálculo do comprometimento da renda diferente do apontado; '
+                    WHEN passo = 0 THEN 'Histórico de crédito incompatível com a idade;'
+                ELSE
+                    'nenhuma'
+				END
+                );
+			SET passo = passo - 1;
+		ELSE
+			SET passo = passo - 1;
+        END CASE;
+	END WHILE;
+    RETURN resposta;
+END $$
+DELIMITER ;
+
+## UNIÃO DAS TABELAS PARA EXPORTAR
+SELECT tab_unica.person_age AS IDADE,
+	tab_unica.person_income AS RENDA_ANUAL,
+    tab_unica.person_home_ownership AS MORADIA,
+    tab_unica.person_emp_length AS TEMPO_DE_SERVICO,
+    tab_unica.loan_intent AS MOTIVO_EMPRESTIMO,
+    tab_unica.loan_grade AS SCORE_CREDITO,
+    tab_unica.loan_amnt AS VALOR_EMPRESTIMO,
+    tab_unica.loan_int_rate AS TAXA_DE_JUROS,
+    tab_unica.loan_status AS SITUACAO_INADIMPLENCIA,
+    tab_unica.loan_percent_income AS COMPROMETIMENTO_DA_RENDA,
+    tab_unica.cb_person_default_on_file AS HISTORICO_DE_INADIMPLENCIA,
+    tab_unica.cb_person_cred_hist_length AS HISTORICO_DE_CREDITO,
+    
+(CASE WHEN
+/*condição dos dados pessoais*/
+tab_unica.person_id = "" or
+tab_unica.person_age is null or
+tab_unica.person_income is null or
+tab_unica.person_home_ownership = "" or
+tab_unica.person_emp_length is null or
+
+/*Condição para os dados de emprestimos*/
+tab_unica.loan_intent = "" or
+tab_unica.loan_grade = "" or
+tab_unica.loan_amnt is null or
+tab_unica.loan_int_rate is null or
+tab_unica.loan_status is null or
+tab_unica.loan_percent_income = "" or
+tab_unica.loan_percent_income is null or
+
+/*condição dos dados de histórico*/
+tab_unica.cb_person_default_on_file="" or
+tab_unica.cb_person_cred_hist_length is null
+
+THEN
+'não'
+ELSE
+'sim'
+END)
+
+AS  CADASTRO_COMPLETO,
+
+observar(
+1000*(CASE WHEN (tab_unica.person_age>100 OR tab_unica.person_age<13) THEN 1 ELSE 0 END)+
+100*(CASE WHEN (tab_unica.person_age - tab_unica.person_emp_length<13) THEN 1 ELSE 0 END)+
+10*(CASE WHEN (ROUND((tab_unica.loan_amnt/tab_unica.person_income),2)<>tab_unica.loan_percent_income) THEN 1 ELSE 0 END)+
+(CASE WHEN (tab_unica.person_age - tab_unica.cb_person_cred_hist_length<13)THEN 1 ELSE 0 END)
+)
+AS OBSERVACOES
+
+FROM
+(SELECT 
+dados_mutuarios.person_id, dados_mutuarios.person_age,
+dados_mutuarios.person_income, dados_mutuarios.person_home_ownership,
+dados_mutuarios.person_emp_length,
+emprestimos.loan_id, emprestimos.loan_intent,
+emprestimos.loan_grade, emprestimos.loan_amnt,
+emprestimos.loan_int_rate, emprestimos.loan_status,
+emprestimos.loan_percent_income,
+historicos_banco.cb_id, historicos_banco.cb_person_default_on_file,
+historicos_banco.cb_person_cred_hist_length
+FROM
+/*Tabelas Unificadas*/
+(((ids 
+INNER JOIN 
+dados_mutuarios
+ON
+ids.person_id = dados_mutuarios.person_id)
+INNER JOIN
+emprestimos
+ON
+ids.loan_id = emprestimos.loan_id)
+INNER JOIN
+historicos_banco
+ON
+ids.cb_id = historicos_banco.cb_id)) tab_unica
+;
+
+/*OQPQ 8
+O QUE - Tradução dos dados da tabela e exportação para o CSV ;
+PORQUE - Para dar mais inteligibilidade aos dados e gerar um arquivo para o processo de Machine Learning - que ocorrerá no Python;
+COMO - Utilizei o metodo replace para as traduções nas colunas em que se fazi necessário, e exportei com o .
+*/
+
+
+## PASSO 9 - TRADUÇÃO DOS DADOS
+UPDATE dados_mutuarios SET
+	person_home_ownership = REPLACE(person_home_ownership, "Rent", "alugada"),
+	person_home_ownership = REPLACE(person_home_ownership, "Own", "propria"),
+	person_home_ownership = REPLACE(person_home_ownership, "Mortgage", "hipoteca"),
+	person_home_ownership = REPLACE(person_home_ownership, "Other", "outro");
+
+UPDATE emprestimos SET
+	loan_intent = REPLACE(loan_intent, "Personal", "uso_pessoal"),
+    loan_intent = REPLACE(loan_intent, "Homeimprovement", "reforma"),
+    loan_intent = REPLACE(loan_intent, "Venture", "empreendimento"),
+    loan_intent = REPLACE(loan_intent, "Education", "educacao"),
+    loan_intent = REPLACE(loan_intent, "Medical", "saude"),
+    loan_intent = REPLACE(loan_intent, "Debtconsolidation", "quitacao_debitos");
+    
+UPDATE historicos_banco SET
+	cb_person_default_on_file = REPLACE(cb_person_default_on_file,'Y', 's'),
+    cb_person_default_on_file = REPLACE(cb_person_default_on_file,'N', 'n');
+
+## PASSO 10 - EXPORT CSV
+## Após as traduções executei as linhas de comando do "PASSO 8" novamente
+## Exportei através do botão de atalho do MySQL Workbench
+
+
+##PASSO 11 - CORREÇÃO DAS INCONSISTÊNCIAS -- Abordagem 2 
+
+## Após a reunião de retorno onde apresentamos algumas soluções foi orientado que as linhas que possuem campos nulos podem ser excluidas
+## no processo de unir as tabelas, assim ajustei as linhas de execução do "PASSO 8" para a exclusão desses campos.
+
+
+## UNIÃO DAS TABELAS PARA EXPORTAR - excluindo campos nulos
+SELECT tab_unica.person_age AS IDADE,
+	tab_unica.person_income AS RENDA_ANUAL,
+    tab_unica.person_home_ownership AS MORADIA,
+    tab_unica.person_emp_length AS TEMPO_DE_SERVICO,
+    tab_unica.loan_intent AS MOTIVO_EMPRESTIMO,
+    tab_unica.loan_grade AS SCORE_CREDITO,
+    tab_unica.loan_amnt AS VALOR_EMPRESTIMO,
+    tab_unica.loan_int_rate AS TAXA_DE_JUROS,
+    tab_unica.loan_status AS SITUACAO_INADIMPLENCIA,
+    tab_unica.loan_percent_income AS COMPROMETIMENTO_DA_RENDA,
+    tab_unica.cb_person_default_on_file AS HISTORICO_DE_INADIMPLENCIA,
+    tab_unica.cb_person_cred_hist_length AS HISTORICO_DE_CREDITO
+
+FROM
+(SELECT 
+dados_mutuarios.person_id, dados_mutuarios.person_age,
+dados_mutuarios.person_income, dados_mutuarios.person_home_ownership,
+dados_mutuarios.person_emp_length,
+emprestimos.loan_id, emprestimos.loan_intent,
+emprestimos.loan_grade, emprestimos.loan_amnt,
+emprestimos.loan_int_rate, emprestimos.loan_status,
+emprestimos.loan_percent_income,
+historicos_banco.cb_id, historicos_banco.cb_person_default_on_file,
+historicos_banco.cb_person_cred_hist_length
+FROM
+/*Tabelas Unificadas*/
+(((ids 
+INNER JOIN 
+dados_mutuarios
+ON
+ids.person_id = dados_mutuarios.person_id)
+INNER JOIN
+emprestimos
+ON
+ids.loan_id = emprestimos.loan_id)
+INNER JOIN
+historicos_banco
+ON
+ids.cb_id = historicos_banco.cb_id)) tab_unica
+    
+WHERE NOT(
+/*condição dos dados pessoais*/
+tab_unica.person_age is null or
+tab_unica.person_income is null or
+tab_unica.person_home_ownership = "" or
+tab_unica.person_emp_length is null or
+
+/*Condição para os dados de emprestimos*/
+tab_unica.loan_intent = "" or
+tab_unica.loan_grade = "" or
+tab_unica.loan_amnt is null or
+tab_unica.loan_int_rate is null or
+tab_unica.loan_status is null or
+tab_unica.loan_percent_income = "" or
+tab_unica.loan_percent_income is null or
+
+/*condição dos dados de histórico*/
+tab_unica.cb_person_default_on_file="" or
+tab_unica.cb_person_cred_hist_length is null);
+
+## PASSO 12 - EXPORT CSV
+## Exportei através do botão de atalho 'Export' no 'Result Grid' do MySQL Workbench
